e374956ec445ba593e43427167ba283fec54ca4d
Dertko Daniil <64313607+dertkoD@users.noreply.github.com>
2026-04-26 21:46:40 +0300

    +

---
+++ HA2DanielDorPalindrome/HA2DanielDorPalindrome.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+bool isPalindrome(int x)
+{
+    if (x < 0)
+        return false;
+
+    if (x % 10 == 0 && x != 0)
+        return false;
+
+    int temp = x;
+    int reversed = 0;
+    int digit = 0;
+
+    while (temp > reversed)
+    {
+        digit = temp % 10;
+        reversed = reversed * 10 + digit;
+        temp = temp / 10;
+    }
+
+    if (temp == reversed)
+        return true;
+    else if (temp == reversed / 10)
+        return true;
+    else
+        return false;
+}
+
+int main()
+{
+    int x;
+
+    cout << "Enter an integer: ";
+    cin >> x;
+
+    if (isPalindrome(x))
+        cout << "true\n";
+    else
+        cout << "false\n";
+
+    return 0;
+}
